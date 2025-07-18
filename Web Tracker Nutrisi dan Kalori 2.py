b72686e3e5d37385123cd82a9168ce1399bdfd7d
Sabrina Ghaisani <sabrina.ghaisani.aka@gmail.com>
2025-07-18 23:12:49 +0700

    Update Web Tracker Nutrisi dan Kalori 2.py

--- Web Tracker Nutrisi dan Kalori 2.py
+++ Web Tracker Nutrisi dan Kalori 2.py
@@ -5,6 +5,35 @@ import matplotlib.pyplot as plt
 
 # ====== Konfigurasi Layout & Logo ======
 st.set_page_config(
+    st.markdown("""
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+
+    html, body, [class*="css"] {
+        font-family: 'Poppins', sans-serif;
+        background-color: #f7fff8;
+        color: #2e2e2e;
+    }
+
+    .stButton>button {
+        background-color: #a8e6cf;
+        color: black;
+        font-weight: bold;
+        border-radius: 10px;
+        padding: 0.4em 1em;
+    }
+
+    .stButton>button:hover {
+        background-color: #81c784;
+        color: white;
+    }
+
+    .stSidebar {
+        background-color: #e5fce7;
+    }
+    </style>
+""", unsafe_allow_html=True)
+
     page_title="NutriTrack - Web Tracker Nutrisi & Kalori",
     layout="wide",
 )
